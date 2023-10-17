8bb8cd9e4531cf5a6e284cceb77729c34b52fa3e
Aleksandr Prilipko <zverozabr@yandex.ru>
2023-10-17 11:08:35 +0700

    rollback commit

--- phenograph_cluster/app.py
+++ phenograph_cluster/app.py
@@ -2,38 +2,10 @@ import phenograph
 import anndata as ad
 import re
 from anndata import AnnData
-import pickle
 import scanpy as sc
 from scipy.stats import zscore
 from scipy.stats.mstats import winsorize
 import numpy as np
-import pandas as pd
-
-
-def get_absolute_relative(path_, absolute=True, ds=''):
-    if absolute:
-        return path_.replace('%DATA_STORAGE%', ds)
-    else:
-        return path_.replace(ds, '%DATA_STORAGE%')
-
-
-def extract_image_id(path):
-    pattern = re.compile(r'[\/\\]originals[\/\\](\d+)[\/\\]image\.tiff')
-    match = pattern.search(path)
-    if match:
-        return match.group(1)
-    else:
-        return None
-
-
-def combine_adata(adata1, adata2, image_id1, image_id2):
-
-    adata1.obs['image_id'] = [image_id1] * adata1.n_obs
-    adata2.obs['image_id'] = [image_id2] * adata2.n_obs
-
-    combined_adata = ad.concat([adata1, adata2], index_unique=None)
-
-    return combined_adata
 
 
 def phenograph_cluster(
@@ -43,6 +15,7 @@ def phenograph_cluster(
         transformation='arcsin',
         scaling='z-score'
 ):
+
     expdf = adata.to_df()  # Get this anndata from feature extraction output or spex load file input
 
     # data_for_calc = expdf.iloc[:, markers]  # marker selection in spex UI
@@ -50,14 +23,14 @@ def phenograph_cluster(
 
     # Dropdown selection for transformation. Options are 'arcsin', 'log', 'none'
     if transformation == 'arcsin':
-        data_for_calc = np.arcsinh(data_for_calc / 5)
+        data_for_calc = np.arcsinh(data_for_calc/5)
     if transformation == 'log':
         data_for_calc = data_for_calc.apply(lambda x: np.log10(x) if np.issubdtype(x.dtype, np.number) else x)
 
     # Dropdown selection for scaling. Options are 'z-score', 'winsorize', 'none'
-    if scaling == 'z-score':
+    if scaling =='z-score':
         data_for_calc = data_for_calc.apply(zscore)
-    if scaling == 'winsorize':
+    if scaling =='winsorize':
         arr_data_frame_winsorized = winsorize(data_for_calc.to_numpy(), limits=(0, .01)).data
         data_for_calc.iloc[:, :] = arr_data_frame_winsorized
 
@@ -78,40 +51,8 @@ def phenograph_cluster(
     return adata
 
 
-def combine_ann_data(fe_data):
-    all_adatas = []
-    all_obs_data = []
-    for key, value in fe_data.items():
-        adata = value.get('adata')
-        im_id = value.get('im_id')
-
-        if adata is not None and im_id is not None:
-            adata.obs['image_id'] = [im_id] * adata.n_obs
-            all_adatas.append(adata)
-            all_obs_data.append(adata.obs)
-
-    vars_union = set().union(*[set(adata.var_names) for adata in all_adatas])
-    missing_vars_list = [vars_union - set(adata.var_names) for adata in all_adatas]
-
-    all_adatas = [
-        ad.concat([adata, ad.AnnData(np.zeros((adata.n_obs, len(missing_vars))),
-                                     var=pd.DataFrame(index=list(missing_vars)),
-                                     obs=adata.obs)], axis=1)
-        if missing_vars else adata
-        for adata, missing_vars in zip(all_adatas, missing_vars_list)
-    ]
-
-    if len(all_adatas) > 0:
-        combined_adata = ad.concat(all_adatas, index_unique=None, join='outer')
-        combined_obs_data = pd.concat(all_obs_data, ignore_index=True)
-        combined_adata.obs = combined_obs_data
-        return combined_adata
-    else:
-        return None
-
-
-
 def run(**kwargs):
+
     adata = kwargs.get('adata')
     scaling = kwargs.get('scaling')
     transformation = kwargs.get('transformation')
@@ -132,23 +73,8 @@ def run(**kwargs):
     else:
         channel_list = list(range(len(all_channels)))
 
-    feature_extraction_tasks = kwargs.get('tasks_list', '')
-    fe_data = {}
-    for task in feature_extraction_tasks:
-        filename = get_absolute_relative(task.get('result', ''), True, ds=kwargs.get('data_storage', ''))
-        try:
-            with open(filename, "rb") as outfile:
-                current_file_data = pickle.load(outfile)
-                im_id = extract_image_id(current_file_data.get('image_path', ''))
-                fe_data = {**fe_data, task.get('_key'): {**current_file_data, 'im_id': im_id}}
-        except (FileNotFoundError, PermissionError) as e:
-            print(e)
-            continue
-
-    combined_adata = combine_ann_data(fe_data)
-
     adata = phenograph_cluster(
-        combined_adata,
+        adata,
         knn,
         markers=channel_list,
         scaling=scaling,
