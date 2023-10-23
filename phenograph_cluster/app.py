944984ef54c1b7e1f7686786fee4c9611fdaf6c7
Aleksandr Prilipko <zverozabr@yandex.ru>
2023-10-23 11:53:04 +0700

    combined anndata

--- phenograph_cluster/app.py
+++ phenograph_cluster/app.py
@@ -2,10 +2,12 @@ import phenograph
 import anndata as ad
 import re
 from anndata import AnnData
+import pickle
 import scanpy as sc
 from scipy.stats import zscore
 from scipy.stats.mstats import winsorize
 import numpy as np
+import pandas as pd
 
 
 def parse_channel_list(channel_list, all_channels):
@@ -32,6 +34,23 @@ def parse_channel_list(channel_list, all_channels):
 
     return channel_list_int, all_channels
 
+
+def get_absolute_relative(path_, absolute=True, ds=''):
+    if absolute:
+        return path_.replace('%DATA_STORAGE%', ds)
+    else:
+        return path_.replace(ds, '%DATA_STORAGE%')
+
+
+def extract_image_id(path):
+    pattern = re.compile(r'[\/\\]originals[\/\\](\d+)[\/\\]image\.tiff')
+    match = pattern.search(path)
+    if match:
+        return match.group(1)
+    else:
+        return None
+
+
 def phenograph_cluster(
         adata,
         knn,
@@ -39,21 +58,21 @@ def phenograph_cluster(
         transformation='arcsin',
         scaling='z-score'
 ):
-
     expdf = adata.to_df()  # Get this anndata from feature extraction output or spex load file input
 
     data_for_calc = expdf.iloc[:, markers]  # marker selection in spex UI
+    # data_for_calc = expdf.copy()
 
     # Dropdown selection for transformation. Options are 'arcsin', 'log', 'none'
     if transformation == 'arcsin':
-        data_for_calc = np.arcsinh(data_for_calc/5)
+        data_for_calc = np.arcsinh(data_for_calc / 5)
     if transformation == 'log':
         data_for_calc = data_for_calc.apply(lambda x: np.log10(x) if np.issubdtype(x.dtype, np.number) else x)
 
     # Dropdown selection for scaling. Options are 'z-score', 'winsorize', 'none'
-    if scaling =='z-score':
+    if scaling == 'z-score':
         data_for_calc = data_for_calc.apply(zscore)
-    if scaling =='winsorize':
+    if scaling == 'winsorize':
         arr_data_frame_winsorized = winsorize(data_for_calc.to_numpy(), limits=(0, .01)).data
         data_for_calc.iloc[:, :] = arr_data_frame_winsorized
 
@@ -74,8 +93,31 @@ def phenograph_cluster(
     return adata
 
 
-def run(**kwargs):
+def combine_ann_data(fe_data):
+    all_adatas = []
+    all_obs_data = []
+    for key, value in fe_data.items():
+        adata = value.get('adata')
 
+        im_id = value.get('im_id')
+
+        if adata is not None and im_id is not None:
+            adata.obs['image_id'] = [im_id] * adata.n_obs
+            all_adatas.append(adata)
+            all_obs_data.append(adata.obs)
+
+    if len(all_adatas) > 0:
+        combined_obs = pd.concat([adata.obs for adata in all_adatas])
+        combined_X = np.vstack([adata.X for adata in all_adatas])
+        combined_var = all_adatas[0].var
+        combined_adata = AnnData(X=combined_X, obs=combined_obs, var=combined_var)
+
+        return combined_adata
+    else:
+        return None
+
+
+def run(**kwargs):
     adata = kwargs.get('adata')
     scaling = kwargs.get('scaling')
     transformation = kwargs.get('transformation')
@@ -93,8 +135,23 @@ def run(**kwargs):
     else:
         channel_list = list(range(len(all_channels)))
 
+    feature_extraction_tasks = kwargs.get('tasks_list', '')
+    fe_data = {}
+    for task in feature_extraction_tasks:
+        filename = get_absolute_relative(task.get('result', ''), True, ds=kwargs.get('data_storage', ''))
+        try:
+            with open(filename, "rb") as outfile:
+                current_file_data = pickle.load(outfile)
+                im_id = extract_image_id(current_file_data.get('image_path', ''))
+                fe_data = {**fe_data, task.get('_key'): {**current_file_data, 'im_id': im_id}}
+        except (FileNotFoundError, PermissionError) as e:
+            print(e)
+            continue
+
+    combined_adata = combine_ann_data(fe_data)
+
     adata = phenograph_cluster(
-        adata,
+        combined_adata,
         knn,
         markers=channel_list,
         scaling=scaling,
